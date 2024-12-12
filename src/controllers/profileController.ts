068c755b4c0aaf59fa40e0668847022dd7d16174
IVAM GALVAO FILHO <programador.ivam@gmail.com>
2024-12-12 19:40:08 -0300

    add update profile

--- src/controllers/profileController.ts
+++ src/controllers/profileController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { getProfileByUserId } from '../services/profileService';
+import { updateProfileByUserId } from '../services/profileService';
 
 /**
  * Controlador para consultar o perfil de um usuário.
@@ -23,3 +24,27 @@ export const getProfileController = async (req: Request, res: Response) => {
     res.status(500).json({ error: errorMessage });
   }
 };
+
+/**
+ * Controlador para atualizar o perfil de um usuário.
+ * Recebe o idUser como parâmetro na URL e os dados para atualização no corpo da requisição.
+ */
+export const updateProfileController = async (req: Request, res: Response) => {
+    const { userId } = req.params;
+    const updatedData = req.body; // Dados de atualização do perfil
+  
+    try {
+      // Chama o serviço para atualizar o perfil do usuário
+      const updatedProfile = await updateProfileByUserId(Number(userId), updatedData);
+  
+      // Verifica se o perfil foi encontrado e atualizado
+      if (updatedProfile) {
+        res.status(200).json({ message: 'Perfil atualizado com sucesso.', updatedProfile });
+      } else {
+        res.status(404).json({ error: 'Perfil não encontrado para o usuário.' });
+      }
+    } catch (error) {
+      const errorMessage = error instanceof Error ? error.message : 'Erro ao atualizar perfil.';
+      res.status(500).json({ error: errorMessage });
+    }
+  };
--- src/routes/profileRoutes.ts
+++ src/routes/profileRoutes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { getProfileController } from '../controllers/profileController';
+import { updateProfileController } from '../controllers/profileController';
 
 const router = Router();
 
@@ -7,5 +8,11 @@ const router = Router();
  * Rota para consultar o perfil de um usuário pelo userId.
  */
 router.get('/profile/:userId', getProfileController);
+
+/**
+ * Rota para atualizar o perfil de um usuário pelo userId.
+ * Utiliza o método PUT para atualizar os dados.
+ */
+router.put('/profile/:userId', updateProfileController);
 
 export default router;
