a4cbd3a130c064090ecc0c510d2abdc947a8a877
IVAM GALVAO FILHO <programador.ivam@gmail.com>
2024-12-13 19:05:32 -0300

    add function delete requeriment

--- src/controllers/requerimentsController.ts
+++ src/controllers/requerimentsController.ts
@@ -4,29 +4,29 @@ import { AnalisysHaveRequirements } from "../models/analisysHaveRequirementsMode
 
 export class RequirementsController {
     static async createRequirement(req: Request, res: Response) {
-            try {
-                const { idAnalisys, ...requirementData } = req.body;
+        try {
+            const { idAnalisys, ...requirementData } = req.body;
 
-                if (!idAnalisys) {
-                    return res.status(400).json({ error: "idAnalisys is required." });
-                }
-
-                // Criação do requisito
-                const requirement = await Requirements.create(requirementData);
-
-                // Criação da associação na tabela AnalisysHaveRequirements
-                await AnalisysHaveRequirements.create({
-                    idAnalisys,
-                    idRequirements: requirement.idRequirements
-                });
-
-                res.status(201).json(requirement);
-            } catch (error) {
-                const errorMessage = error instanceof Error ? error.message : 'Erro ao criar o requisito.';
-                res.status(500).json({ error: errorMessage });
+            if (!idAnalisys) {
+                return res.status(400).json({ error: "idAnalisys is required." });
             }
+
+            // Criação do requisito
+            const requirement = await Requirements.create(requirementData);
+
+            // Criação da associação na tabela AnalisysHaveRequirements
+            await AnalisysHaveRequirements.create({
+                idAnalisys,
+                idRequirements: requirement.idRequirements
+            });
+
+            res.status(201).json(requirement);
+        } catch (error) {
+            const errorMessage = error instanceof Error ? error.message : 'Erro ao criar o requisito.';
+            res.status(500).json({ error: errorMessage });
         }
-    
+    }
+
     static async getAllRequirements(req: Request, res: Response) {
         try {
             const requirements = await Requirements.findAll();
@@ -78,7 +78,14 @@ export class RequirementsController {
                 return res.status(404).json({ message: "Requirement not found" });
             }
 
+            // Excluir associação na tabela AnalisysHaveRequirements
+            await AnalisysHaveRequirements.destroy({
+                where: { idRequirements: requirement.idRequirements }
+            });
+
+            // Excluir o requisito
             await requirement.destroy();
+
             res.status(204).send();
         } catch (error) {
             const errorMessage = error instanceof Error ? error.message : 'Erro ao excluir o requisito.';
